9154346168b4b68548ef9084832ba3bcfbf238dc
Eldric <eldriclim93@gmail.com>
2018-05-22 15:28:20 +0800

    Create deliverer done

--- app/controllers/deliverers_controller.rb
+++ app/controllers/deliverers_controller.rb
@@ -5,4 +5,26 @@ class DeliverersController < ApplicationController
     @deliverer = Deliverer.find(params[:id])
   end
 
+  # GET /deliverers/new
+  def new
+    @deliverer = Deliverer.new
+  end
+
+  # POST /deliverers
+  def create
+    @deliverer = Deliverer.new(deliverer_params)
+
+    if @deliverer.save
+      # handle
+      redirect_to @deliverer
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def deliverer_params
+    params.require(:deliverer).permit(:name, :phone, :vehicle, :active)
+  end
+
 end
--- app/models/deliverer.rb
+++ app/models/deliverer.rb
@@ -2,9 +2,8 @@ class Deliverer < ApplicationRecord
 
   VAILD_PHONE_REGEX = /\d+/
 
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50, message: "Name is longer than 50 characters." }
   validates :vehicle, presence: true
-  validates :phone, presence: true, format: { with: VAILD_PHONE_REGEX }, uniqueness: true
-  validates :active, presence: true
+  validates :phone, presence: true, format: { with: VAILD_PHONE_REGEX, message: "Only integer is allowed." }
 
 end
