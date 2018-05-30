3075b5737ac42792e421daaa0d51984e3e0ffb70
Eldric <eldriclim93@gmail.com>
2018-05-30 14:05:29 +0800

    Remove unused views

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,10 @@
 module AssignmentsHelper
+  def print_date_range(d1, d2)
+    if d1.to_date == d2.to_date
+      tag.strong("On ") + d1.strftime("%d %B %Y")
+    else
+      tag.strong("From ") + d1.strftime("%d %B %Y") +
+        tag.strong(" to ") + d2.strftime("%d %B %Y")
+    end
+  end
 end
