fd7adc0be083785b09e65c54a574364884bfbc26
Eldric <eldriclim93@gmail.com>
2018-06-01 16:46:40 +0800

    Change Service specs to use rspec-given

--- spec/services/shift_assignment_service_spec.rb
+++ spec/services/shift_assignment_service_spec.rb
@@ -3,80 +3,56 @@ require 'rails_helper'
 RSpec.describe ShiftAssignmentService do
 
   describe "#perform" do
-    before do
-      @deliverers = FactoryGirl.create_list(:deliverer,3)
-      @shift = FactoryGirl.create(:shift)
+    Given!(:deliverers) { FactoryGirl.create_list(:deliverer,3) }
+    Given!(:shift) { FactoryGirl.create(:shift) }
 
-      @service1 = ShiftAssignmentService.new(@deliverers[0].id, @shift.id)
-    end
+    Given!(:service1) { ShiftAssignmentService.new(deliverers[0].id, shift.id) }
 
-    subject { ShiftAssignmentService.new }
 
     context "when deliverer or shift id is nil" do
-      before do
-        # Setup erroneous services
-        @service_deliverer_nil = ShiftAssignmentService.new(nil, @shift.id)
-        @service_shift_nil = ShiftAssignmentService.new(@deliverers[0].id, nil)
-      end
+      # Setup erroneous services
+      Given!(:service_deliverer_nil) { ShiftAssignmentService.new(nil, shift.id) }
+      Given!(:service_shift_nil) { ShiftAssignmentService.new(deliverers[0].id, nil) }
 
-      it "adds to errors" do
+      Then { expect(service_deliverer_nil.perform).to eq(false) }
+      And { expect(service_deliverer_nil.errors).to include("Please create some Deliverers and Shifts first.") }
 
-        expect(@service_deliverer_nil.perform).to eq(false)
-        expect(@service_deliverer_nil.errors).to include("Please create some Deliverers and Shifts first.")
-
-        expect(@service_shift_nil.perform).to eq(false)
-        expect(@service_shift_nil.errors).to include("Please create some Deliverers and Shifts first.")
-
-      end
+      And { expect(service_shift_nil.perform).to eq(false) }
+      And { expect(service_shift_nil.errors).to include("Please create some Deliverers and Shifts first.") }
     end
 
     context "when Shift is already maxed out" do
-      before do
-        # Setup services to be assigned
-        @service2 = ShiftAssignmentService.new(@deliverers[1].id, @shift.id)
-        @service3 = ShiftAssignmentService.new(@deliverers[2].id, @shift.id)
+      # Setup services to be assigned
+      Given!(:service2) { ShiftAssignmentService.new(deliverers[1].id, shift.id) }
+      Given!(:service3) { ShiftAssignmentService.new(deliverers[2].id, shift.id) }
 
-        # Max out Shift: 0/2 -> 2/2
-        @service1.perform
-        @service2.perform
-      end
+      # Max out Shift: 0/2 -> 2/2
+      When { service1.perform }
+      When { service2.perform }
 
-      it "adds to error" do
-        expect(@service3.perform).to eq(false)
-        expect(@service3.errors).to include("Shift count has already maxed out!")
-      end
+      Then { expect(service3.perform).to eq(false) }
+      And { expect(service3.errors).to include("Shift count has already maxed out!") }
     end
 
     context "when Assignment already exist" do
-      before do
-        # Add pre-existing service
-        @service1.perform
-      end
+      # Add pre-existing service
+      When { service1.perform }
 
-      it "adds to error" do
-        expect(@service1.perform).to eq(false)
-        expect(@service1.errors).to include("Assignment already exist!")
-      end
+      Then { expect(service1.perform).to eq(false) }
+      And { expect(service1.errors).to include("Assignment already exist!") }
     end
 
     context "when error in saving" do
-      before do
-        # Setup erroneous services
-        @service_erroneous = ShiftAssignmentService.new(-1, @shift.id)
-      end
+      # Setup erroneous services
+      Given!(:service_erroneous) { ShiftAssignmentService.new(-1, shift.id) }
 
-      it "adds to error" do
-        expect(@service_erroneous.perform).to eq(false)
-        expect(@service_erroneous.errors).to include("Error in assigning shift!")
-
-      end
+      Then { expect(service_erroneous.perform).to eq(false) }
+      And { expect(service_erroneous.errors).to include("Error in assigning shift!") }
     end
 
     context "when successful" do
-      it "adds to success" do
-        expect(@service1.perform).to eq(true)
-        expect(@service1.success).to include("A new assignment has been made!")
-      end
+      Then { expect(service1.perform).to eq(true) }
+      And { expect(service1.success).to include("A new assignment has been made!") }
     end
   end
 
