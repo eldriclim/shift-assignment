701752adbdeddbb973d28f1c699237746da80554
Eldric <eldriclim93@gmail.com>
2018-06-04 17:59:14 +0800

    Edit .rubocop.yml, rubocop on helpers

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,13 +1,23 @@
 module AssignmentsHelper
+  # rubocop:disable Metrics/AbcSize
   def print_date_range(range)
-    d1 = range.begin
-    d2 = range.end
+    d1 = range.begin.to_date
+    d2 = range.end.to_date
 
-    if d1.to_date == d2.to_date
-      tag.strong("On ") + d1.strftime("%d %B %Y")
+    if d1 == d2
+      bold('On ') + date_output(d1)
     else
-      tag.strong("From ") + d1.strftime("%d %B %Y") +
-        tag.strong(" to ") + d2.strftime("%d %B %Y")
+      bold('From ') + date_output(d1) +
+        bold(' to ') + date_output(d2)
     end
+  end
+  # rubocop:enable Metrics/AbcSize
+
+  def date_output(date)
+    date.strftime('%d %B %Y')
+  end
+
+  def bold(string)
+    tag.strong(string)
   end
 end
--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,11 +1,11 @@
 module SharedHelper
   def flash_ul(message)
-    if message.kind_of?(Array)
-      tag.ul{
+    if message.is_a?(Array)
+      tag.ul do
         message.each do |m|
           concat tag.li(m)
         end
-      }
+      end
     else
       message
     end
