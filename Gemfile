c9057cbb33f9a7efe1860608cacdec3d5f91d54d
Eldric Lim <eldriclim93@gmail.com>
2018-06-18 16:39:36 +0800

    Setup bullet gem

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'factory_girl_rails'
 gem 'database_cleaner'
 gem 'rails-controller-testing'
 gem 'coveralls'
+gem 'bullet'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'active_record'
+require 'bullet'
+
 require 'coveralls'
 require 'simplecov'
 
@@ -105,4 +108,15 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  if Bullet.enable?
+    config.before(:each) do
+      Bullet.start_request
+    end
+
+    config.after(:each) do
+      Bullet.perform_out_of_channel_notifications if Bullet.notification?
+      Bullet.end_request
+    end
+  end
 end
